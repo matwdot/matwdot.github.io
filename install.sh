62d8013b2249370d31fc67d2436ba6948cebe12d
matwdot <matheusw.linux@gmail.com>
2025-12-07 20:32:29 -0300

    testando script

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# Este script pode ser executado diretamente com:
+# curl -sSL https://meusite.github.io/install | bash
+
+set -euo pipefail
+
+echo "Deu certo"
+# seu código aqui
