e95dfef5f2cf50df89e686ee2a342a1b0f67158d
matwdot <matheusw.linux@gmail.com>
2026-01-06 09:00:07 -0300

    repaginação no site de apresentação do projeto.

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Este script pode ser executado diretamente com:
-# curl -sSL https://meusite.github.io/install | bash
-
-set -euo pipefail
-
-echo "Deu certo"
-# seu código aqui
